ec4bea9018d77ff3891ddf646ee57c279f40694c
Surinda Sarakum <surindasarakum18@gmail.com>
2021-03-28 14:42:08 +0700

    Add project files.

---
+++ Exam 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Enter a:");
+		int a = Convert.ToInt32(Console.ReadLine());
+		int x = a;
+		Console.WriteLine("Enter b:");
+		int b = Convert.ToInt32(Console.ReadLine());
+		int y = b;
+
+		while (true)
+		{
+			if (x == y)
+			{
+				break;
+			}
+			else
+			{
+				if (x < y)
+				{
+					x = x + a;
+				}
+				else
+				{
+					y = y + b;
+				}
+			}
+		}
+
+		Console.WriteLine("result X:" + x);
+		Console.WriteLine("result y:" + y);
+		Console.WriteLine("result a:" + a);
+		Console.WriteLine("result b:" + b);
+	}
+}
